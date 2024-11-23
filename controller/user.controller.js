3da1e6f1c590e039a580ed77fa951edc3148a557
ashirwadexe <ashirwadbappy@gmail.com>
2024-11-23 10:54:03 +0530

    registration logic

---
+++ controller/user.controller.js
@@ -0,0 +1,38 @@
+import { User } from "../models/user.model";
+import bcrypt from "bcrypt";
+
+//REGISTRATION Logic
+const register = async (req, res) => {
+    try {
+        const { name, email, password} = req.body;
+        if(!name || !email || !password) {
+            return res.status(400).json({
+                message: "Something is missing!!!",
+                success: false
+            });
+        }
+
+        const user = await User.findOne({email});
+        if(user) {
+            return res.status(400).json({
+                message: "user already exits !",
+                success: false
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+            name, 
+            email, 
+            password: hashedPassword
+        });
+
+        return res.status(200).json({
+            message: "Account created successfully",
+            success: true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
