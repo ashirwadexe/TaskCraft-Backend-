6244d45ab83139cb1a67349806f4fe1f9c97235a
ashirwadexe <ashirwadbappy@gmail.com>
2024-11-23 11:29:34 +0530

    login logic

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,3 +1,4 @@
+import { JsonWebTokenError } from "jsonwebtoken";
 import { User } from "../models/user.model";
 import bcrypt from "bcrypt";
 
@@ -40,4 +41,57 @@ export const register = async (req, res) => {
           success: false,
         });
       }
+};
+
+//USER LOGIN Logic
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            });
+        }
+
+        let user = await User.findOne({email});
+        if(!user) {
+            return res.status(404).json({
+                message: "User not found",
+                success: false
+            });
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatched) {
+            return res.status(400).json({
+                message: "Incorrect Password",
+                success: false
+            });
+        }
+
+        const tokenData = {
+            userId: user._id
+        }
+
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn: '1d'});
+
+        user = {
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        }
+
+        return res.status(200).cookie("token", token, {maxAge: 1*24*60*60*1000, httpsOnly: true, sameSite: 'strict'}).json({
+            message: "User Logged In !!!",
+            success: true,
+            user
+        });
+    } catch (error) {
+        console.error("Login Error:", error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            success: false,
+        });
+    }
 };
